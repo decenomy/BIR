6ec432dbb97fc47f837e42b54b72f885ad496d9c
Alex <alex@birake.com>
2020-06-16 00:41:16 +0300

    fix replaces

--- src/pivxd.cpp
+++ src/pivxd.cpp
@@ -119,7 +119,7 @@ bool AppInit(int argc, char* argv[])
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "PIVX server starting\n");
+            fprintf(stdout, "BirakeCoin server starting\n");
 
             // Daemonize
             pid_t pid = fork();
--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2764,7 +2764,7 @@ UniValue encryptwallet(const UniValue& params, bool fHelp)
     // slack space in .dat files; that is bad if the old data is
     // unencrypted private keys. So:
     StartShutdown();
-    return "wallet encrypted; pivx server stopping, restart to run with encrypted wallet. The keypool has been flushed, you need to make a new backup.";
+    return "wallet encrypted; BirakeCoin server stopping, restart to run with encrypted wallet. The keypool has been flushed, you need to make a new backup.";
 }
 
 UniValue lockunspent(const UniValue& params, bool fHelp)
